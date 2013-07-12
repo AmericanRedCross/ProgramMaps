2308f7d4856665e53d8ecfcafcbf537064987ae9
bivilin <bivilin@gmail.com>
2013-07-12 10:40:04 -0400

    First Commit Latin America Programs

---
+++ lacPrograms/arcPrograms.js
@@ -0,0 +1,18 @@
+var arcPrograms = [
+{"COUNTRY":"Bahamas","REGION":"LAC","Program1":"Integrated","Program2":"Community Health","Program3":"Preparedness","Program4":"Organizational Development","Program5":null,"Latitude":24.3196,"Longitude":-76.2765},
+{"COUNTRY":"Belize","REGION":"LAC","Program1":"Integrated","Program2":"Preparedness","Program3":"","Program4":null,"Program5":null,"Latitude":17.4789,"Longitude":-88.5162},
+{"COUNTRY":"Chile","REGION":"LAC","Program1":"Disaster Recovery","Program2":"","Program3":"","Program4":null,"Program5":null,"Latitude":-35.1234,"Longitude":-71.572},
+{"COUNTRY":"Colombia","REGION":"LAC","Program1":"Integrated","Program2":"Refugee Care","Program3":"Preparedness","Program4":"Community Health","Program5":null,"Latitude":5.0689,"Longitude":-74.5263},
+{"COUNTRY":"Costa Rica","REGION":"LAC","Program1":"Integrated","Program2":"Preparedness","Program3":"Water and Sanitation","Program4":null,"Program5":null,"Latitude":9.9247,"Longitude":-84.078},
+{"COUNTRY":"Ecuador","REGION":"LAC","Program1":"Integrated","Program2":"Preparedness","Program3":"Refugee Care","Program4":"Community Health","Program5":null,"Latitude":-0.15,"Longitude":-78.35},
+{"COUNTRY":"El Salvador","REGION":"LAC","Program1":"Organizational Development","Program2":"Disaster Recovery","Program3":"Integrated","Program4":"Preparedness","Program5":"Water and Sanitation","Latitude":13.7176,"Longitude":-89.0279},
+{"COUNTRY":"Guatemala","REGION":"LAC","Program1":"Disaster Recovery","Program2":"Water and Sanitation","Program3":"","Program4":null,"Program5":null,"Latitude":14.7162,"Longitude":-90.6185},
+{"COUNTRY":"Guyana","REGION":"LAC","Program1":"Integrated","Program2":"Preparedness","Program3":"Community Health","Program4":null,"Program5":null,"Latitude":6.349,"Longitude":-58.582},
+{"COUNTRY":"Honduras","REGION":"LAC","Program1":"Disaster Recovery","Program2":"Water and Sanitation","Program3":"","Program4":null,"Program5":null,"Latitude":14.7806,"Longitude":-87.4384},
+{"COUNTRY":"Jamaica","REGION":"LAC","Program1":"Community Health","Program2":"","Program3":"","Program4":null,"Program5":null,"Latitude":18.1315,"Longitude":-77.2736},
+{"COUNTRY":"Nicaragua","REGION":"LAC","Program1":"Integrated","Program2":"Preparedness","Program3":"Disaster Recovery","Program4":"Water and Sanitation","Program5":null,"Latitude":12.5969,"Longitude":-85.8795},
+{"COUNTRY":"Panama","REGION":"LAC","Program1":"Integrated","Program2":"Preparedness","Program3":"Water and Sanitation","Program4":null,"Program5":null,"Latitude":8.7515,"Longitude":-79.8772},
+{"COUNTRY":"Paraguay","REGION":"LAC","Program1":"Preparedness","Program2":"","Program3":"","Program4":null,"Program5":null,"Latitude":-24.9217,"Longitude":-57.3708},
+{"COUNTRY":"Peru","REGION":"LAC","Program1":"Integrated","Program2":"Preparedness","Program3":"Organizational Development","Program4":null,"Program5":null,"Latitude":-11.2583,"Longitude":-75.1374},
+{"COUNTRY":"Saint Lucia","REGION":"LAC","Program1":"Preparedness","Program2":"","Program3":"","Program4":null,"Program5":null,"Latitude":13.8989,"Longitude":-60.9682}
+]
