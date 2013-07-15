ff2ec06c711cb4f5119443d9ce0c02ec5610cc3a
Daniel Joseph <djosep@gmail.com>
2013-07-15 12:05:46 -0400

    First bits of 'on click' functionality

--- WorldPrograms/js/main.js
+++ WorldPrograms/js/main.js
@@ -13,6 +13,26 @@ var map = L.map('map', {
     maxBounds: bounds
 });
 
+// create custom control for display of arc logo, map info, etc
+var info = L.control();
+
+info.onAdd = function (map) {
+    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+    this.update();
+    return this._div;
+};
+
+// method to update the control based on feature properties passed
+
+// when clearing the info.update on reset highlight it passes nothing to this function which messes it up
+info.update = function (props) {
+    var infoContent = '<img class=arclogo src="images/redcross-logo.png" /><br>' + '<h4>International Programs 2013</h4><hr>' + (props ? '<b>' + props.name : 'Click on a country') + "</p><ul class='programList'>";
+    this._div.innerHTML = infoContent
+};
+
+info.addTo(map);
+
+
 function mapStyle(feature) {
     return{
     fillColor: "#808080",
@@ -25,26 +45,42 @@ function mapStyle(feature) {
 
 var featureEvents = function (feature, layer) {
     layer.on({
-        mouseover: highlightingEvent,
-        mouseout: resetHighlight,        
+        // mouseover: highlightingEvent,
+        // mouseout: resetHighlight,
+        click: countryClick
     });       
 }
 
-function highlightingEvent (e) {
+// function highlightingEvent (e) {
+//     var country = e.target;
+//     country.setStyle({
+//         weight: 5,
+//         color: '#666',
+//         dashArray: '',
+//         fillOpacity: 0.7
+//     });
+//     if (!L.Browser.ie && !L.Browser.opera) {
+//         country.bringToFront();
+//     }      
+// }
+
+// function resetHighlight (e) {
+//     geojson.resetStyle(e.target);    
+// }
+
+function countryClick (e) {
+    geojson.resetStyle(geojson);
     var country = e.target;
     country.setStyle({
         weight: 5,
-        color: '#666',
+        color: '#fed53c',
         dashArray: '',
         fillOpacity: 0.7
     });
     if (!L.Browser.ie && !L.Browser.opera) {
         country.bringToFront();
-    }      
-}
-
-function resetHighlight (e) {
-    geojson.resetStyle(e.target);    
+    }
+    info.update(country.feature.properties);  
 }
 
 function getWorld() {
