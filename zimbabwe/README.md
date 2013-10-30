5ff68ecc30342a7f275a3279b7a1a849dcbc9500
danbjoseph <danbjoseph@gmail.com>
2013-10-30 17:17:41 -0400

    super code cleanup and markers working

--- zimbabwe/README.md
+++ zimbabwe/README.md
@@ -1,12 +1,11 @@
-Mapfolio
-========
+Title
+=====
 
-#### [http://americanredcross.github.io/Mapfolio/](http://americanredcross.github.io/Mapfolio/) ####
+#### [http://americanredcross.github.io/ProgramMaps/zimbabwe/](http://americanredcross.github.io/ProgramMaps/zimbabwe/) ####
 
-Interactive portfolio to exhibit map products of the [American Red Cross](http://redcross.org/) International Services GIS team. 
+Produced by the [American Red Cross](http://redcross.org/) International Services GIS team. 
 
 **Technology Used:**
-- [Bootstrap v3.0.0](http://getbootstrap.com/)
 - [Leaflet v0.6.2](http://leafletjs.com/)
 - [Leaflet.markercluster](http://github.com/Leaflet/Leaflet.markercluster)
 - [jQuery v1.10.1](http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js)
--- zimbabwe/js/main.js
+++ zimbabwe/js/main.js
@@ -1,28 +1,19 @@
-var windowHeight = $(window).height();
-
-
 //disclaimer text
 function showDisclaimer() {
     window.alert("The maps on this page do not imply the expression of any opinion on the part of the American Red Cross concerning the legal status of a territory or of its authorities.");
 }
 
-
-// MAP SHIT 
 var latrines = [];
 var markersBounds = [];
-var displayedPoints = [];
-var markers = new L.MarkerClusterGroup();
 
-var countryStyle = {
-    color: '#fff',
-    weight: 1,
-    fillColor: '#d7d7d8',
-    fillOpacity: 1,
-    clickable: false
-};
+var markers = new L.MarkerClusterGroup({
+    showCoverageOnHover:false, 
+    spiderfyDistanceMultiplier:3,
+    maxClusterRadius: 30
+});
 
 var centroidOptions = {
-    radius: 8,
+    radius: 7,
     fillColor: "#ED1B2E",
     color: "#FFF",
     weight: 2.5,
@@ -30,86 +21,36 @@ var centroidOptions = {
     fillOpacity: 1
 };
 
-var cloudmadeUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a> | &copy; <a href="http://redcross.org" title="Red Cross" target="_blank">Red Cross</a> 2013';
-var cloudmade = L.tileLayer(cloudmadeUrl, {attribution: attribution});
+var tileLayerUrl = 'http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
+var attribution = 'Map data &copy; <a href="http://openstreetmap.org" target="_blank">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/" target="_blank">CC-BY-SA</a> | Map style by <a href="http://hot.openstreetmap.org" target="_blank">H.O.T.</a> | &copy; <a href="http://redcross.org" title="Red Cross" target="_blank">Red Cross</a> 2013 | <a title="Disclaimer" onClick="showDisclaimer();">Disclaimer</a>';
+var tileLayer = L.tileLayer(tileLayerUrl, {attribution: attribution});
 
 var map = L.map('map', {   
-    zoom: 4,
+    zoom: 0,
     // scrollWheelZoom: false,
-    layers: [cloudmade]
+    layers: [tileLayer]
 });
-// cloudmade.setOpacity(0); 
-
-// change display accordingly to the zoom level
-// function mapDisplay() {
-//     var remove = {fillOpacity:0, opacity:0}
-//     var add = {fillOpacity:1, opacity:1}
-//     map.on('viewreset', function() {
-//         if (map.getZoom() < 6) {
-//             cloudmade.setOpacity(0);
-//             geojson.setStyle(add);
-//         } else {
-//             geojson.setStyle(remove);
-//             cloudmade.setOpacity(1);
-//         }
-//     })
-// }
-
-// on marker click open modal
-// function centroidClick (e) {
-//     var thumbnail_id = "#" + e.target.feature.properties.thumbnail_id;    
-//     if ($(thumbnail_id).hasClass("ONLINE")) {
-//         url = $(thumbnail_id).find('a').attr('href');
-//         window.open(url, '_blank');
-//     } else {
-//         callModal(thumbnail_id);
-//     }    
-// }
 
 // on marker mouseover
-// function displayName(e) {   
-//     var target = e.target;
-//     target.openPopup();   
-// }
+function displayPopup(e) {   
+    var target = e.target;
+    target.openPopup();   
+}
 // on marker mouseout
-// function clearName(e) {    
-//     var target = e.target;
-//     target.closePopup();    
-// }
-
-// beginning of function chain to initialize map
-function getWorld() {
-    $.ajax({
-        type: 'GET',
-        url: 'data/worldcountries.json',
-        contentType: 'application/json',
-        dataType: 'json',
-        timeout: 10000,
-        success: function(json) {
-            worldcountries = json;
-            countries = new L.layerGroup().addTo(map);
-            geojson = L.geoJson(worldcountries,{
-                style: countryStyle
-            }).addTo(countries);
-            getLatrines();
-        },
-        error: function(e) {
-            console.log(e);
-        }
-    });
+function clearPopup(e) {    
+    var target = e.target;
+    target.closePopup();    
 }
 
 function getLatrines() {
     $.ajax({
         type: 'GET',
-        url: 'data/latrineData.json',
+        url: 'data/ZWE_toilets.json',
         contentType: 'application/json',
         dataType: 'json',
         timeout: 10000,
         success: function(data) {
-            formatCentroids(data);
-            // mapDisplay();
+            formatData(data);            
         },
         error: function(e) {
             console.log(e);
@@ -117,17 +58,12 @@ function getLatrines() {
     });
 }
 
-function formatCentroids(data){
+function formatData(data){
     $.each(data, function(index, item) {
-        var gpsString = item.GPS.replace(/\s/g, "");
-        var longStart = gpsString.indexOf("E");
-        var itemLat = parseFloat(gpsString.replace("S","-").substr(0,longStart-1)).toString();
-        var itemLong = parseFloat(gpsString.substr(longStart + 1)).toString();    
-        var latlng = [itemLat, itemLong];
+        var latlng = [item.LONG, item.LAT];
         var mapCoord = {
             "type": "Feature",
             "properties": {
-                "name": item.BENEFICIARY,
                 "community": item.COMMUNITY, 
                 "vulnerability": item.VULNERABILITY
 
@@ -143,70 +79,38 @@ function formatCentroids(data){
 }
 
 function markersToMap(){
-    map.removeLayer(markers);
-    markers = new L.MarkerClusterGroup({showCoverageOnHover:false, spiderfyDistanceMultiplier:3,});
-      
-
     marker = L.geoJson(latrines, {
         pointToLayer: function (feature, latlng) {
             return L.circleMarker(latlng, centroidOptions);
         },
-        // onEachFeature: function(feature, layer) {
-        //     var thumbnail_id = "#" + feature.properties.thumbnail_id;
-        //     var popupContent = $(thumbnail_id).find('.caption').html();
-        //     var popupOptions = {
-        //         'minWidth': 30,
-        //         'offset': [0,-10],
-        //         'closeButton': false,
-        //     }; 
-        //     layer.bindPopup(popupContent, popupOptions);
-        //     layer.on({
-        //         click: centroidClick,
-        //         mouseover: displayName,
-        //         mouseout: clearName,
-        //     });   
-        // }            
+        onEachFeature: function(feature, layer) {
+            var community = feature.properties.community;
+            var vulnerability = feature.properties.vulnerability;            
+            var popupContent = feature.geometry.coordinates[0] + ", " + feature.geometry.coordinates[1]
+            var popupOptions = {
+                'minWidth': 30,
+                'offset': [0,-10],
+                'closeButton': false,
+            }; 
+            layer.bindPopup(popupContent, popupOptions);
+            layer.on({
+                // click: centroidClick,
+                mouseover: displayPopup,
+                mouseout: clearPopup,
+            });   
+        }            
     });
     markers.addLayer(marker);
     markers.addTo(map);
     markersBounds = markers.getBounds();
-    markersBounds._northEast.lat += 5;
-    markersBounds._northEast.lng += 5;
-    markersBounds._southWest.lat -= 5;
-    markersBounds._southWest.lat -= 5;
     map.fitBounds(markersBounds);
 } 
-
-
-$(window).resize(function(){    
-    map.fitBounds(markersBounds);    
-    windowHeight = $(window).height();
-})
-
 
 
 // reset map bounds using Zoom to Extent button
 function zoomOut() {
     map.fitBounds(markersBounds);
 }
-
-// tweet popup
-$('.twitterpopup').click(function(event) {
-    var width  = 575,
-        height = 400,
-        left   = ($(window).width()  - width)  / 2,
-        top    = ($(window).height() - height) / 2,
-        url    = this.href,
-        opts   = 'status=1' +
-                 ',width='  + width  +
-                 ',height=' + height +
-                 ',top='    + top    +
-                 ',left='   + left;
-
-    window.open(url, 'twitter', opts);
-
-    return false;
-});
 
 // start function chain to initialize map
 getLatrines();
